ab92efc181c27fcd7d64c82ae05efca88820adbe
Valentin <dreamofandroid@gmail.com>
2018-02-04 22:41:06 +0600

    modify css

--- js/menu.js
+++ js/menu.js
@@ -3,21 +3,21 @@ window.onload = function(){
 	var arr1 = document.querySelector(".header-menu-item1");
 	var arr2 = document.querySelector(".header-menu-item2");
 	var arr3 = document.querySelector(".header-menu-item3");
-    document.querySelector(".header-menu").onclick = function(){         
-            console.log(isPressed);
+    document.querySelector(".header-menu").onclick = function(){ 
         if(isPressed){
             arr1.style.transform = "rotate(0deg)";
             arr3.style.transform = "rotate(0deg)";
-            $(".header nav").slideUp(100);
+            $(".header nav").slideUp(300);
             isPressed = false;
         }else{
             arr1.style.transform = "rotate(-35deg)";
             arr3.style.transform = "rotate(35deg)";
             //$(".header nav").css("display", "block");
-            $(".header nav").slideDown(100);
+            $(".header nav").slideDown(400);
             isPressed = true;
         }
             
             	    
     }
 };
+
